b146306deb29d5a71c7af3d20e4df9cc09a78749
Brian <bellbrian14@gmail.com>
2013-02-18 22:21:20 -0600

    Added ability to connect to database

--- DegreePlanner/DegreePlanner/Form1.cs
+++ DegreePlanner/DegreePlanner/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace DegreePlanner
 {
@@ -15,6 +16,28 @@ namespace DegreePlanner
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=instance39020.db.xeround.com;Port=6887;Database=csv_db;Uid=admin;Pwd=password";
+            MySqlConnection con = new MySqlConnection(connectionString);
+            string query = "SELECT * FROM `tbl_name` WHERE COURSENUMBER = 271 LIMIT 0,30";
+            MySqlCommand cmd = new MySqlCommand(query);
+            MySqlDataReader reader = null;
+            string result = null;
+            con.Open();
+            cmd.Connection = con;
+            reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                result = reader.GetString(2) + '\n';
+                textBox1.Text += result;
+            }
+
+
+            con.Close();
         }
     }
 }
