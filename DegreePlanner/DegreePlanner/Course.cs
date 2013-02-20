5fc8c668e0e6d72939501aa54bc75f810b9c3a46
arfreitas12 <arfreitas12@tamu.edu>
2013-02-20 16:42:38 -0600

    Changes to course file

--- DegreePlanner/DegreePlanner/Course.cs
+++ DegreePlanner/DegreePlanner/Course.cs
@@ -8,6 +8,19 @@ namespace DegreePlanner
 {
     class Course
     {
+        public string Description;
+        public int CourseNum;
+        public int Hours;
+        public string PreReq;
+        public string Deptartment;
+        public Course()
+        {
+            this.Deptartment = "";
+            this.Description = "";
+            this.CourseNum = 0;
+            this.PreReq = "";
+            this.Hours = 0;
+        }
         private string name;
         private List<Course> prerequisites;
 
@@ -23,7 +36,7 @@ namespace DegreePlanner
 
         public override string ToString()    //called ToString so that the name representation is given in a listBox
         {
-            return name;
+            return this.Deptartment + " " + this.CourseNum;
         }
 
         public void addPrerequisite(Course pre)
