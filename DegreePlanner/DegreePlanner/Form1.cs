a93e4e476fe62d95c2094f94631396187e77564c
Brian <bellbrian14@gmail.com>
2013-02-23 14:42:38 -0600

    -

--- DegreePlanner/DegreePlanner/Form1.cs
+++ DegreePlanner/DegreePlanner/Form1.cs
@@ -78,34 +78,6 @@ namespace DegreePlanner
             (sender as ListBox).DoDragDrop((sender as ListBox).SelectedItem, DragDropEffects.Move);
             (sender as ListBox).Items.Remove((sender as ListBox).SelectedItem);
         }
-
-        private void getRequiredCourses()
-        {
-            /*Course csce111 = new Course("CSCE", 111, "", );
-            Course csce121 = new Course("CSCE 121");
-            Course csce181 = new Course("CSCE 181");
-            Course csce221 = new Course("CSCE 221");
-            Course csce222 = new Course("CSCE 222");
-            Course csce312 = new Course("CSCE 312");
-            Course csce314 = new Course("CSCE 314");
-            Course csce313 = new Course("CSCE 313");
-            Course csce315 = new Course("CSCE 315");
-
-            freshmanFall.Items.Add(csce111);
-
-            freshmanSpring.Items.Add(csce121);
-            freshmanSpring.Items.Add(csce181);
-
-            sophomoreFall.Items.Add(csce221);
-            sophomoreFall.Items.Add(csce222);
-
-            sophomoreSpring.Items.Add(csce312);
-            sophomoreSpring.Items.Add(csce314);
-
-            juniorFall.Items.Add(csce313);
-            juniorFall.Items.Add(csce315);*/
-        }
-
-
+            
     }
 }
